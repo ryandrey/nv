097e5f55fc3d423a0a90efdd8572edb90327204e
ryandrey <r.andrew200@mail.ru>
2021-12-16 16:04:28 +0300

    cutting wav

--- nv/datasets/LJSpeechDataset.py
+++ nv/datasets/LJSpeechDataset.py
@@ -5,10 +5,11 @@ import torch
 
 class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
 
-    def __init__(self, root, limit=None, *args, **kwargs):
+    def __init__(self, root, size=None, limit=None, *args, **kwargs):
         super().__init__(root=root)
         self._index = list(range(super().__len__()))
         self.limit = limit
+        self.size = size
         if limit is not None:
             random.seed(42)
             random.shuffle(self._index)
@@ -16,6 +17,10 @@ class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
 
     def __getitem__(self, index: int):
         waveform, _, _, _ = super().__getitem__(self._index[index])
+        if self.size is not None:
+            rand = random.randint(0, waveform.size(-1) - self.size)
+            waveform = waveform[:, rand:rand + self.size]
+
         waveform_length = torch.tensor([waveform.shape[-1]]).int()
 
         return waveform, waveform_length

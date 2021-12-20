6f76cfced780a4001660d5509e40eea2664c6095
ryandrey <r.andrew200@mail.ru>
2021-12-20 19:15:10 +0300

    test init

--- test.py
+++ test.py
@@ -1,41 +1,63 @@
 import torch
+import argparse
+import collections
 import torchaudio
-from torch.nn.utils.rnn import pad_sequence
+from nv.utils.parse_config import ConfigParser
 
-from nv.model import FastSpeech
-from nv.model import Vocoder
+import nv.model as module_arch
+from nv.featurizer import MelSpectrogramConfig, MelSpectrogram
+
+
+def main(config):
+    device = torch.device('cuda:0')
+    generator = config.init_obj(config["arch"], module_arch)
+    checkpoint = torch.load(config.resume, device)
+    generator.load_state_dict(checkpoint["state_dict"])
+    generator.to(device)
+    featurizer = MelSpectrogram(MelSpectrogramConfig).to(device)
+    generator.eval()
+    paths = ["1.wav",
+             "2.wav",
+             "3.wav"]
+
+    for i, path in enumerate(paths):
+        true_wav, _ = torchaudio.load(path)
+        spec = featurizer(true_wav.to(device))
+        output = generator(spec)
+        torchaudio.save(f"{i + 1}_pred.wav", output.cpu(), sample_rate=22050)
+
 
 if __name__ == "__main__":
-    texts = [
-        "A defibrillator is a device that gives a high energy electric shock to the heart of someone who is in cardiac arrest",
-        "Massachusenv Institute of Technology may be best known for its math, science and engineering education",
-        "Wasserstein distance or Kantorovich Rubinstein metric is a distance function defined between probability distributions on a given metric space",
-        "Give me some points please"
-    ]
-
-    device = torch.device('cuda:0')
-    model = FastSpeech(
-        ntoken=51,
-        d_model=384,
-        d_k=384,
-        num_attn_layers=2,
-        conv_d=1536,
-        kernel_size=3,
-        nlayers=6,
-        num_mels=80,
-        dropout=0.1
+    args = argparse.ArgumentParser(description="PyTorch Template")
+    args.add_argument(
+        "-c",
+        "--config",
+        default=None,
+        type=str,
+        help="config file path (default: None)",
     )
-    checkpoint = torch.load("nv_model.pth", device)
-    model.load_state_dict(checkpoint["state_dict"])
-    model.to(device)
-    model.eval()
+    args.add_argument(
+        "-r",
+        "--resume",
+        default=None,
+        type=str,
+        help="path to latest checkpoint (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--device",
+        default=None,
+        type=str,
+        help="indices of GPUs to enable (default: all)",
+    )
 
-    vocoder = Vocoder().to(device)
-    tokenizer = torchaudio.pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
-
-    for i, text in enumerate(texts):
-        tokens, token_lengths = tokenizer(text)
-        output = model(tokens.to(device), None)
-        pred_wav = vocoder.inference(output.transpose(-1, -2)).cpu()
-        torchaudio.save(f"{i + 1}.wav", pred_wav, sample_rate=22050)
-
+    # custom cli options to modify configuration from default values given in json file.
+    CustomArgs = collections.namedtuple("CustomArgs", "flags type target")
+    options = [
+        CustomArgs(["--lr", "--learning_rate"], type=float, target="optimizer;args;lr"),
+        CustomArgs(
+            ["--bs", "--batch_size"], type=int, target="data_loader;args;batch_size"
+        ),
+    ]
+    config = ConfigParser.from_args(args, options)
+    main(config)
